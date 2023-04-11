0805deddb74cd4f261894b759ee699e9517382b0
Master__Harvey <ianpadraicharvey@gmail.com>
2023-04-11 16:43:07 -0600

    full screen bg color

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,7 +6,7 @@ import UseScrollToTop from '../hooks/useScrollToTop';
 function MyApp({ Component, pageProps }) {
 	return (
 		<AnimatePresence>
-			<div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
+			<div className=" bg-secondary-light dark:bg-primary-dark transition duration-300 min-h-screen">
 				<DefaultLayout>
 					<Component {...pageProps} />
 				</DefaultLayout>
