3afc5667e741838d5f0bdedf1a0c0424549f344d
Master__Harvey <ianpadraicharvey@gmail.com>
2023-04-11 03:06:03 -0600

    adjustments, just needs new project data and photos

--- components/projects/ProjectsGrid.jsx
+++ components/projects/ProjectsGrid.jsx
@@ -35,64 +35,15 @@ function ProjectsGrid() {
 			</div>
 
 			<div className="mt-10 sm:mt-16">
-				<h3
-					className="
-                        font-general-regular 
-                        text-center text-secondary-dark
-                        dark:text-ternary-light
-                        text-md
-                        sm:text-xl
-                        mb-3
-                        "
-				>
-					Search projects by title or filter by category
-				</h3>
 				<div
-					className="
-                        flex
-                        justify-between
-                        border-b border-primary-light
-                        dark:border-secondary-dark
-                        pb-3
-                        gap-3
-                        "
+					className="flex justify-between border-b border-primary-light dark:border-secondary-dark pb-3 gap-3"
 				>
-					<div className="flex justify-between gap-2">
-						<span
-							className="
-                                hidden
-                                sm:block
-                                bg-primary-light
-                                dark:bg-ternary-dark
-                                p-2.5
-                                shadow-sm
-                                rounded-xl
-                                cursor-pointer
-                                "
-						>
-							<FiSearch className="text-ternary-dark dark:text-ternary-light w-5 h-5"></FiSearch>
-						</span>
+					<div className="flex ml-auto gap-2">
 						<input
 							onChange={(e) => {
 								setSearchProject(e.target.value);
 							}}
-							className="
-                                ont-general-medium 
-                                pl-3
-                                pr-1
-                                sm:px-4
-                                py-2
-                                border 
-                            border-gray-200
-                                dark:border-secondary-dark
-                                rounded-lg
-                                text-sm
-                                sm:text-md
-                                bg-secondary-light
-                                dark:bg-ternary-dark
-                                text-primary-dark
-                                dark:text-ternary-light
-                                "
+							className="ont-general-medium pl-3 pr-1 sm:px-4 py-2 border border-gray-200 dark:border-secondary-dark rounded-lg text-sm sm:text-md bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light"
 							id="name"
 							name="name"
 							type="search"
@@ -100,6 +51,11 @@ function ProjectsGrid() {
 							placeholder="Search Projects"
 							aria-label="Name"
 						/>
+						<span
+							className="hidden sm:block bg-primary-light dark:bg-ternary-dark p-2.5 shadow-sm rounded-xl cursor-pointer"
+						>
+							<FiSearch className="text-ternary-dark dark:text-ternary-light w-5 h-5"></FiSearch>
+						</span>
 					</div>
 				</div>
 			</div>
@@ -107,11 +63,11 @@ function ProjectsGrid() {
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-5">
 				{selectProject
 					? selectProjectsByCategory.map((project, index) => {
-							return <ProjectSingle key={index} {...project} />;
-					  })
+						return <ProjectSingle key={index} {...project} />;
+					})
 					: projectsData.map((project, index) => (
-							<ProjectSingle key={index} {...project} />
-					  ))}
+						<ProjectSingle key={index} {...project} />
+					))}
 			</div>
 		</section>
 	);
