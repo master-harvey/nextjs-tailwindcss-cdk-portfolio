97d655fcdd70dce2dc411ecaad3cf8dc65299bb9
Master__Harvey <ianpadraicharvey@gmail.com>
2023-04-17 16:53:44 -0600

    removed search bar

--- components/projects/ProjectsGrid.jsx
+++ components/projects/ProjectsGrid.jsx
@@ -30,21 +30,6 @@ function ProjectsGrid(props) {
 				</p>
 			</div>
 
-			<div className="mt-6 sm:mt-10">
-				<div className="flex justify-between border-b border-primary-light dark:border-secondary-dark pb-3 gap-3">
-					<div className="flex ml-auto gap-2">
-						<input
-							className="ont-general-medium pl-3 pr-1 sm:px-4 py-2 border border-gray-200 dark:border-secondary-dark rounded-lg text-sm sm:text-md bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light"
-							onChange={(e) => setSearchProject(e.target.value)}
-							id="name" name="name" type="search" required="" placeholder="Search Projects" aria-label="Name"
-						/>
-						<span className="hidden sm:block bg-primary-light dark:bg-ternary-dark p-2.5 shadow-sm rounded-xl cursor-pointer">
-							<FiSearch className="text-ternary-dark dark:text-ternary-light w-5 h-5"></FiSearch>
-						</span>
-					</div>
-				</div>
-			</div>
-
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-5">
 				{props.featured
 					? selectProjectsByFeatured.map((project, index) => {
