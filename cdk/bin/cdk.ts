85b292d2567c711d6979da6e36c185384fcca145
Master__Harvey <ianpadraicharvey@gmail.com>
2023-05-20 14:43:46 -0600

    description

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,4 +4,4 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'portfolioJS', { stackName: "portfolioJS-notify" });
+new CdkStack(app, 'portfolioJS', { stackName: "portfolioJS-notify", description: "SNS topic and lambda to power the portfolio contact form" });
